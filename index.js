ebcb40f0b5be96cf4521eda76ebaad3866d07a66
Your Name <you@example.com>
2025-12-05 00:25:53 +0900

    m2

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const a = 10
 const b = 20
 const c = 30
-const d = 40
+const f = 50
 
-console.log("a + b + c + d>> ", a + b + d + c)
+console.log("a + b + c - f >> ", a + b + c - f)
+console.log("a + b >> ", a + b)
 
-
-function sum(a, b) { return a + b}
-
+console.log("a + b + c - f >> ", a + b + c - f)
